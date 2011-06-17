1eeef73f50efc024ae3d85e8d83f3d6dc0439198
Joey Aghion <joey@aghion.com>
2011-06-16 21:53:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caniwatchitwithmyparents::Application.config.secret_token = '7fd1a16334eed591fe9a0e5c1c069c090ea2a8d5ad7c1b56eadcb1218cfb93ab918318d74744f5cd40db9fd0d11c9161c6ed6a1eeba8d600ed1c87d3574abe8d'
