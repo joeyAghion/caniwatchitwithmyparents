ffd1442cf401b1fef793fe73a44f3cda40263887
Joey Aghion <joey@aghion.com>
2011-06-21 09:50:33 -0400

    database-insensitive case-insensitive movie name comparison

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
   def self.find_matching(s)
-    self.where("name LIKE ?", "%#{s}%").limit(10)
+    self.where("UPPER(name) LIKE UPPER(?)", "%#{s}%").limit(10)
   end
 end
